0166bc0283aa43d952e86376ba890b3f836f4512
JoeYoung94 <yang1133@uwindsor.ca>
2017-03-04 19:04:24 -0500

    modify

--- 656pro_server.c
+++ 656pro_server.c
@@ -10,166 +10,43 @@
 #include <arpa/inet.h>
 #include <ctype.h>
 #include <time.h>
+#include <fcntl.h>
+#include <termio.h>
 
 
 /*
 *Created by Yunqiao Yang
 *Date Feb 17, 2017
-*Description: This is the server side of teh project voice control application which can recognize the specifiec commands so that it can control the Arduino hardware
+*Modified by Yunqiao Yang
+*Date: March 3, 2017
+*Description: This is the server side of the project. Bying implementing it using TCP, it can receive the messages from the client and then send the command to Arduino 
+*board.
+*Description2: This is the refined server side. The main functionality is receiving the message from the clients
+*, which is recognized previously. And then the command will be sent to the serial port ACM0, Arduino.
 */
 
 
 #define MAXBUF 1024
 
-void commandRecog(char string[]); // declaration of command line recognition
+int portCommunication(void);  //the functionality of communicating with serial port.
 
-
-
-
-void commandRecog(char string[])   //command line recognition function
-
+int portCommunication(void)
 {
+	int fd;
 	
-	//char string[200],
-	char matchcase[4][10];
-	FILE *fp;  //file pointer to write time stamp and string log to log.txt  
-
-    	//define each key ..
-    	strcpy(matchcase[0], "open");
-    	strcpy(matchcase[1], "light");
-    	strcpy(matchcase[2], "close");
-    	strcpy(matchcase[3], "dark");
-    	int i = 0, j = 0;
-
- 
-
-      	/*Scanning string*/
-	
-    //	printf("Enter string: ");
-
-   	 //do
-
-    	//{
-
-       		//fflush(stdin);
-
-        	//c = getchar();
-
-        	//string[i++] = tolower(c);
-
- 
-
-    	//} 
-    	//while (c != '\n');
-
-    	//string[i - 1] = '\0';
-	
-	printf("\nThe target string is: %s\n\n", string);
-
-	fp = fopen("log.txt", "a+");
-
-	if(fp == 0)
+	fd = open("/dev/ttyACM0", O_RDWR | O_NOCTTY | O_NONBLOCK);
+	if(fd == -1)
 	{
-		printf("Cannot open the log file.\n");
-		exit(0);
-
+		printf("cannot open the serial port...\n");
+		return -1;
 	}
-
 	
-
-	fseek(fp, 0, SEEK_END);
-
-	fwrite(string, strlen(string), 1, fp);
-
-	fprintf(fp,"\t");	
-
-	time_t timep;
-
-	time(&timep);
-
-	printf("Inputing time:%s\n" , ctime(&timep));
-
-	fwrite(ctime(&timep), strlen(ctime(&timep)), 1, fp);
-
-	fprintf(fp,"\n");
-
-	fclose(fp);
-
-    	/*Scanning substring*/
-
-    	/*printf("Enter key: ");
-
-    	i = 0;
-
-    	do
-
-    	{
-
-        	fflush(stdin);
-
-        	c = getchar();
-
-        	matchcase[i++] = tolower(c);
-
-    	} 
-    	while (c != '\n');
-
-    	matchcase[i - 1] = '\0';*/
-
-    	int counter;   // this is the parameter to count the times to recognition
-    	for(counter = 0; counter < 4; counter++)
-  	 	{
-    		for (i = 0,j = 0; i < strlen(string) - strlen(matchcase[counter]) + 1; i++)
-
-    		{
-
-       			int index = i;
-
-        		if (string[i] == matchcase[counter][j])
-
-        		{
-
-           			 do
-
-            			{
-
-               				i++;
-                			j++;
-
-            			} while(j != strlen(matchcase[counter]) && string[i] == matchcase[counter][j]);
-
-           			if (j == strlen(matchcase[counter]))
-
-            			{
-
-					printf("%s:\n", matchcase[counter]);               	 			
-					printf("Match found from position %d to %d.\n", index + 1, i);
-
-                			break;
-
-            			}
-
-           		 	else
-
-            			{
-
-                			i = index + 1;
-
-                			j = 0;
-
-            			}
-
-       	 		}
-  		}
-
-    	}
-
-    	//printf("No substring match found in the string.\n");
-
- 
-
-    	//return 0;
-
+	else
+	{
+		//printf("open serial port succeed...\n");
+		fcntl(fd, F_SETFL, 0);
+		return fd;
+	}
 }
 
 
@@ -181,6 +58,7 @@ int main(int argc , char* argv[])
 	struct sockaddr_in my_addr, their_addr;
 	unsigned int myport, lisnum;     //the port and the number of listening queue
 	char buf[MAXBUF + 1];
+	int fd; //this parameter is used to communicate with serial port.
 
 	if (argv[2])
 		myport = atoi(argv[2]);  // changing the command line to integer, which can be used into the port.
@@ -219,6 +97,8 @@ int main(int argc , char* argv[])
 	if (bind(sockfd, (struct sockaddr*) &my_addr, sizeof(struct sockaddr)) == -1)
 	{
 		perror("bind");
+		close(sockfd);
+		//close(new_fd);
 		exit(EXIT_FAILURE);
 	}	
 
@@ -226,6 +106,7 @@ int main(int argc , char* argv[])
 	if(listen(sockfd, lisnum) == -1)
 	{
 		perror("listen");
+		close(sockfd);
 		exit(EXIT_FAILURE);
 
 	}
@@ -234,6 +115,8 @@ int main(int argc , char* argv[])
 	{
 		//block waiting for connection, when it becomes succeed, returen new socket descriptor, their_Addr is used to store the sockaddr of client side
 		perror("accept");
+		close(sockfd);
+		close(new_fd);
 		exit(EXIT_FAILURE);
 
 	}
@@ -245,33 +128,13 @@ int main(int argc , char* argv[])
 	if((pid = fork()) == -1)   // failed to create a new process
 	{
 		perror("fork");
+		close(sockfd);
+		close(new_fd);
 		exit(EXIT_FAILURE);
 
 	}
 	else if(pid == 0)    // the child process is used to receieve message from the client
 	{
-		/*while(1)
-		{
-			bzero(buf, MAXBUF + 1);
-			printf("Please input the message to send: \n");
-			fgets(buf, MAXBUF + 1, stdin);
-			if(!strncasecmp(buf, "quit", 4))
-			{
-				printf("I will close the connection!\n");
-				break;
-
-			}
-			len = send(new_fd, buf, strlen(buf) - 1, 0); // send the message to the client
-			if(len < 0)
-			{
-				printf("message '%s' send failed! error code is %d, error message is %s.\n", buf, errno, strerror(errno));
-				break;
-
-			}
-	
-
-		}*/
-		
 		while(1)
 		{
 			bzero(buf, MAXBUF + 1);
@@ -279,11 +142,16 @@ int main(int argc , char* argv[])
 			if(len > 0)
 			{
 				printf("Message received successful: '%s', %d Byte received.\n", buf, len);
+				printf("Now sending message to Arduino board...\n");
+				fd = portCommunication();
+				write(fd, buf, strlen(buf));
+				close(fd);
+				printf("Done...\n");
 			
 			}
 			else if(len < 0)
 			{
-				printf("REceieving message failed. Error code is: %d, error message is '%s'\n", errno, strerror(errno));
+				printf("Receieving message failed. Error code is: %d, error message is '%s'\n", errno, strerror(errno));
 				break;
 
 			}
@@ -299,41 +167,26 @@ int main(int argc , char* argv[])
 	}
 	else       // the parent process is used to listening other clients
 	{
-		/*while(1)
-		{
-			bzero(buf, MAXBUF + 1);
-			len = recv(new_fd, buf, MAXBUF, 0);
-			if(len > 0)
-				printf("Message received successful: '%s', %d Byte received.\n", buf, len);
-			else if(len < 0)
-			{
-				printf("REceieving message failed. Error code is: %d, error message is '%s'\n", errno, strerror(errno));
-				break;
-
-			}
-			else
-			{
-				printf("The other one closed and quited.\n");
-				break;
-
-			}
-			
-		}*/
+		
 		
 		while(1)
 		{
 			if((pid = fork()) == -1)
 			{
 				perror("fork");
+				close(sockfd);
+				close(new_fd);
 				exit(EXIT_FAILURE);
 			}
 			
-			else if (pid > 0)
+			else if (pid > 0)   //the infinite fork() to handle more clients...
 			{
 				if((new_fd = accept(sockfd, (struct sockaddr*)&their_addr, &len)) == -1)
 				{
 				//block waiting for connection, when it becomes succeed, returen new socket descriptor, their_Addr is used to store the sockaddr of client side
 					perror("accept");
+					close(sockfd);
+					close(new_fd);
 					exit(EXIT_FAILURE);
 
 				}
@@ -351,11 +204,16 @@ int main(int argc , char* argv[])
 					{
 						printf("Commands from %s, port %d, socket %d\n", inet_ntoa(their_addr.sin_addr), ntohs(their_addr.sin_port), new_fd);
 						printf("Message received successful: '%s', %d Byte received.\n", buf, len);
-						commandRecog(buf);
+						printf("Now sending message to Arduino...\n");
+						fd = portCommunication();
+						write(fd, buf, strlen(buf));
+						close(fd);
+						printf("Done...\n");
+						
 					}
 					else if(len < 0)
 					{
-						printf("REceieving message failed. Error code is: %d, error message is '%s'\n", errno, strerror(errno));
+						printf("Receieving message failed. Error code is: %d, error message is '%s'\n", errno, strerror(errno));
 						break;
 
 					}
